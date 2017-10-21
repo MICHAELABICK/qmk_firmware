24e2b470f37e168237701a5457308e19c18408e6
Michael Bick <MichaelBick@lawn-128-61-68-51.lawn.gatech.edu>
2017-10-21 10:01:47 -0400

    Added shift functionality to spacebar

--- keyboards/planck/keymaps/mbick/keymap.c
+++ keyboards/planck/keymaps/mbick/keymap.c
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,               KC_T,    KC_Y,    KC_U,               KC_I,            KC_O,            KC_P,          KC_BSPC},
   {CTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,               KC_G,    KC_H,    KC_J,               KC_K,            KC_L,            KC_SCLN,       RCTL_T(KC_ENT)},
   {OSM(MOD_LSFT),       KC_Z,    KC_X,    KC_C,    KC_V,               KC_B,    KC_N,    KC_M,               KC_COMM,         KC_DOT,          KC_SLSH,       RSFT_T(KC_QUOT)},
-  {ADJUST,        KC_LCTL, KC_LALT, KC_LGUI, LT(_LOWER, KC_TAB), KC_SPC,  KC_SPC,  LT(_RAISE, KC_ENT), RGUI_T(KC_LEFT), RALT_T(KC_DOWN), RCTL_T(KC_UP), KC_RGHT}
+  {ADJUST,        KC_LCTL, KC_LALT, KC_LGUI, LT(_LOWER, KC_TAB), SFT_T(KC_SPC),  RSFT_T(KC_SPC),  LT(_RAISE, KC_ENT), RGUI_T(KC_LEFT), RALT_T(KC_DOWN), RCTL_T(KC_UP), KC_RGHT}
 },
 
 /* Colemak
