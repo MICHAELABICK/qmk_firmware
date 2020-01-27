208f74990b5f49ab2e86c47150a09615a1fc568f
Michael Bick <mafbick@gmail.com>
2020-01-26 23:37:17 -0500

    Added a basic vim mode to mbick keymap

--- keyboards/planck/keymaps/mbick/keymap.c
+++ keyboards/planck/keymaps/mbick/keymap.c
@@ -21,6 +21,7 @@ enum planck_layers {
   _QWERTY,
   _NUMS,
   _SYMB,
+  _VIM,
 };
 
 /* enum planck_keycodes { */
@@ -33,10 +34,10 @@ enum planck_layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_planck_grid(
-    KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R, KC_T,   KC_Y,           KC_U, KC_I,    KC_O,    KC_P,    KC_BSPC,
-    LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F, KC_G,   KC_H,           KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V, KC_B,   KC_N,           KC_M, KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT),
-    XXXXXXX,        KC_LCTL, KC_LALT, KC_LGUI, NUMS, KC_SPC, HYPR_T(KC_SPC), SYMB, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_TAB,         KC_Q,     KC_W,    KC_E,    KC_R, KC_T,           KC_Y,           KC_U, KC_I,    KC_O,    KC_P,    KC_BSPC,
+    LCTL_T(KC_ESC), KC_A,     KC_S,    KC_D,    KC_F, KC_G,           KC_H,           KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT,        KC_Z,     KC_X,    KC_C,    KC_V, KC_B,           KC_N,           KC_M, KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT),
+    XXXXXXX,        TG(_VIM), KC_LALT, KC_LGUI, NUMS, HYPR_T(KC_SPC), HYPR_T(KC_SPC), SYMB, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 [_NUMS] = LAYOUT_planck_grid(
@@ -51,6 +52,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX, KC_GRV,  XXXXXXX, KC_LCBR, KC_LBRC, KC_MINS, KC_EQL,  KC_RBRC, KC_RCBR, KC_BSLS, KC_PIPE, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_UNDS, KC_PLUS, XXXXXXX, XXXXXXX, KC_PGUP, KC_PGUP, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+),
+
+[_VIM] = LAYOUT_planck_grid(
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, TG(_VIM), XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,    KC_RGHT, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX
 ),
 
 };
